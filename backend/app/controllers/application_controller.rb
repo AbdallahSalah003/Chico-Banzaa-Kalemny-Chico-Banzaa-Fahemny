791022f7dd477b758ec310310a1801aaa0bd26b5
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2025-11-28 23:28:48 +0200

    allow custom fields with devise

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  #this make sure Devise accepts our custom fields
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    #allow these fields during signup
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+      :username, :first_name, :last_name, :birth_date, 
+      :gender, :city, :address, :role
+    ])
+    
+    # allow fields during profile Update
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      :first_name, :last_name, :birth_date, :gender, 
+      :city, :address
+    ])
+  end
 end
