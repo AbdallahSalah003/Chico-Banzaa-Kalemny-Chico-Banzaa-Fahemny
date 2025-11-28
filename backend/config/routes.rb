e66425f4c3812645b3ab6f06312cef766ecefcd0
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2025-11-28 23:26:01 +0200

    add routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,11 +1,30 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Devise Auth routes
+  # We map the controllers to handle JSON responses for login/signup
+  devise_for :users, 
+             path: '', 
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               registration: 'signup'
+             },
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :matches, only: [:index, :show, :create, :update]
+      resources :stadiums, only: [:index, :create]
+      
+      resources :tickets, only: [:create, :destroy]
+      
+      patch '/users/:id/approve', to: 'users#approve'
+      resources :users, only: [:index, :destroy]
+    end
+  end
+  
+  #WebSocket Route 
+  mount ActionCable.server => '/cable'
 end
